620e06153fc31fad0c8242678226c5789d80cb69
Roman <67958372+TRK08@users.noreply.github.com>
2021-07-13 12:21:22 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <Jokes />
+  </div>
+</template>
+
+<script>
+import "./assets/css/style.css";
+import Jokes from "./pages/Jokes.vue";
+
+export default {
+  components: { Jokes },
+  name: "App",
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/pages/Jokes.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Search />
+    <JokeList />
+  </div>
+</template>
+
+<script>
+import JokeList from "../components/JokeList.vue";
+import Search from "../components/Search";
+export default {
+  components: { Search, JokeList },
+  name: "JokeList",
+};
+</script>
+
+<style scoped>
+</style>
